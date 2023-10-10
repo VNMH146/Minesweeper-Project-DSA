9cbaa0e3c90308ea019aa74c486090311338d94d
Đồng Minh Thắng <ititiu20303@student.hcmiu.edu.vn>
2023-10-10 09:56:19 +0700

    10/10/2023

--- minesweeper.js
+++ minesweeper.js
@@ -3,3 +3,17 @@ var rows = 8;
 var columns = 8;
 
 var minesCount = 5;
+var minesLocation = [];
+
+var titlesClicked  = 0;
+var flagEnabled = false;
+
+var gameOver = false;
+
+window.onload = function() {
+    startGame();
+}
+
+function startGame() {
+    
+}
