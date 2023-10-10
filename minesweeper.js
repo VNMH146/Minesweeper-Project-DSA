1ce44a9926d2478baded66eb00a2e5ba7884d7e2
Đồng Minh Thắng <ititiu20303@student.hcmiu.edu.vn>
2023-10-10 10:06:43 +0700

    17:45

--- minesweeper.js
+++ minesweeper.js
@@ -5,7 +5,7 @@ var columns = 8;
 var minesCount = 5;
 var minesLocation = [];
 
-var titlesClicked  = 0;
+var tilesClicked  = 0;
 var flagEnabled = false;
 
 var gameOver = false;
@@ -15,5 +15,18 @@ window.onload = function() {
 }
 
 function startGame() {
-    
+    document.getElementById("mines-count").innerText = minesCount;
+
+    for(let r = 0; r < rows; r++) {
+        let rows = [];
+        for(let c = 0; c < columns; c++) {
+            let tile = document.createElement("div");
+            tile.id = r.toString() + "-" + c.toString();
+            tile.addEventListener("click", clickTile);
+            document.getElementById("board").append(tile);
+            row.push(tile);
+        }
+        board.push(row);
+    }
+    console.log(board);
 }
